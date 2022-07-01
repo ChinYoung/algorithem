76ec6703b6765804c0b1c2a103cb1e3e0cad5ffb
yang <yang_qin@epam.com>
2022-07-01 13:37:54 +0800

    update: sort-heap

--- sort-heap.py
+++ sort-heap.py
@@ -6,42 +6,72 @@ from typing import List
 class Z_Heap:
     def __init__(self, nums: List[int]) -> None:
         self.__data = nums
-        self.max_index = len(nums) - 1
 
+    # 节点比较方法, 可重写, 相等返回0, 胜出返回1, 否则返回-1
+    @classmethod
+    def compare(cls, first:int, second:int):
+        if first == second:
+            return 0
+        return 1 if first > second else -1
+
+    @property
+    def max_index(self):
+        return len(self.__data) - 1
+
+    @property
+    def node_count(self):
+        return len(self.__data)
+
+    # 堆顶元素
+    @property
+    def best(self):
+        return self.__data[0]
+
+    # 获取左子节点index
     def get_left(self, index:int):
         return 2 * index + 1
 
+    # 获取右子节点index
     def get_right(self, index:int):
         return 2 * index + 2
 
+    # 获取父节点index
     def get_parent(self, index:int):
         if index == 0:
             return -1
         return int((index - 1)/2)
 
+    # 从最后一个节点开始, 进行排序
     def heapify(self):
-        if len(self.__data) < 2:
+        if self.node_count < 2:
             return
-        last = len(self.__data) - 1
-        for i in reversed(range(last)):
+        for i in reversed(range(self.node_count)):
             parent = self.get_parent(i)
             if parent != -1:
                 self.sift_down(parent)
 
+    # 取出堆顶节点
     def sift(self):
         if self.max_index == -1:
             raise Exception("empty heap")
         if self.max_index == 0:
             max = self.__data.pop()
-            self.max_index = -1
             return max
         self.__data[0], self.__data[self.max_index] = self.__data[self.max_index], self.__data[0]
         max = self.__data.pop()
-        self.max_index = len(self.__data) - 1
         self.sift_down(0)
         return max
 
-    # 下沉, 确保节点比父节点小
+    # 替换堆顶元素
+    def replace(self, num):
+        if self.node_count == 0:
+            return
+        replaced = self.__data[0]
+        self.__data[0] = num
+        self.sift_down(0)
+        return replaced
+
+    # 下沉
     def sift_down(self, index):
         node_val = self.__data[index]
         left_index = self.get_left(index)
@@ -52,33 +82,32 @@ class Z_Heap:
         right_index = self.get_right(index)
         if right_index <= self.max_index:
             right_val = self.__data[right_index]
-            if right_val > comparing_val:
+            if self.compare(right_val, comparing_val) == 1:
                 comparing_val = right_val
                 comparing_index = right_index
-        if comparing_val > node_val:
+        if self.compare(comparing_val, node_val) == 1:
             self.__data[comparing_index], self.__data[index] = self.__data[index], self.__data[comparing_index]
             self.sift_down(comparing_index)
 
+    # 上浮
     def sift_up(self, index):
         node_val = self.__data[index]
         parent_index = self.get_parent(index)
         if parent_index != -1:
             parent_val = self.__data[parent_index]
-            if node_val > parent_val:
+            if self.compare(node_val, parent_val) == 1:
                 self.__data[parent_index], self.__data[index] = self.__data[index], self.__data[parent_index]
                 self.sift_up(parent_index)
 
+    # 添加节点
     def push(self, num):
         self.__data.append(num)
-        self.max_index = len(self.__data) - 1
         self.sift_up(self.max_index)
 
-    def get_max(self):
-        return self.__data[0]
-
+    # 打印堆
     def show(self):
         if self.max_index == -1:
-            print("")
+            print("Heap is empty")
             return
         node_queue = queue.Queue()
         node_queue.put(0)
@@ -95,73 +124,40 @@ class Z_Heap:
                 if right_child <= self.max_index:
                     node_queue.put(right_child)
             print("\n+")
-        print("+"*30)
+        print("+"*30, "\n\n")
 
+    # 从队列中取出当前层的所有节点
     def get_layer(self, queue):
         cur_all = []
         while not queue.empty():
             cur_all.append(queue.get())
         return cur_all
 
+
+
+
+class A_Heap(Z_Heap):
+
+    @classmethod
+    def compare(cls, first: int, second: int):
+        return super().compare(first, second) * -1
+
+
+
+
 if __name__ == "__main__":
-    z = Z_Heap([1, 2, 3, 44, 55, 66, 77, 33, 20, 12, 19, 35, 36, 21])
+    z = A_Heap([1, 2, 3, 44, 55, 66, 77, 33, 20, 12, 19, 35, 36, 21])
     # sort
     z.heapify()
 
-    print(">>max: ", z.get_max())
+    print(">>max: ", z.best)
     z.show()
     # add
     z.push(21)
-    print(">>max: ", z.get_max())
+    print(">>max: ", z.best)
     z.show()
 
-    # remove
-    print(">>removed max", z.sift())
-    z.show()
-    # remove
-    print(">>removed max", z.sift())
-    z.show()
-    # remove
-    print(">>removed max", z.sift())
-    z.show()
-    # remove
-    print(">>removed max", z.sift())
-    z.show()
-    # remove
-    print(">>removed max", z.sift())
-    z.show()
-    # remove
-    print(">>removed max", z.sift())
-    z.show()
-    # remove
-    print(">>removed max", z.sift())
-    z.show()
-    # remove
-    print(">>removed max", z.sift())
-    z.show()
-    # remove
-    print(">>removed max", z.sift())
-    z.show()
-    # remove
-    print(">>removed max", z.sift())
-    z.show()
-    # remove
-    print(">>removed max", z.sift())
-    z.show()
-    # remove
-    print(">>removed max", z.sift())
-    z.show()
-    # remove
-    print(">>removed max", z.sift())
-    z.show()
-    # remove
-    print(">>removed max", z.sift())
-    z.show()
-    # remove
-    print(">>removed max", z.sift())
-    z.show()
-    # remove
-    print(">>removed max", z.sift())
-    z.show()
-
-
+    for i in range(z.node_count):
+        # remove
+        print(">>removed max", z.sift())
+        z.show()
--- tree.py
+++ tree.py
@@ -1,3 +1,30 @@
+# 处理
+def handle(node: map):
+    print(node['value'])
+
+# 获取左子节点
+def get_left(current_index: int, tree: list) -> map:
+    try:
+        left_index = 2 * current_index + 1
+        return {'index': left_index, 'value': tree[left_index]}
+    except IndexError:
+        return None
+    except Exception:
+        raise Exception
+
+# 获取右子节点
+def get_right(index: int, tree: list) -> map:
+    try:
+        right_index = 2 * index + 2
+        return {'index': right_index, 'value': tree[right_index]}
+    except IndexError:
+        return None
+    except Exception:
+        raise Exception
+
+
+
+
 # 深度优先， 利用栈
 def dfs(tree: list):
     stack = list()
@@ -6,7 +33,7 @@ def dfs(tree: list):
     root_node = {'value': root_value, 'index': root_index}
     dfs_search(root_node, stack, tree)
 
-
+# 搜索, 如果节点存在子节点, 优先递归搜索子节点, 否则入栈, 确保最底深的节点优先处理
 def dfs_search(current_node: map, stack: list, tree: list):
     left_node = get_left(current_node['index'], tree)
     if left_node:
@@ -20,7 +47,9 @@ def dfs_search(current_node: map, stack: list, tree: list):
     handle(current_node)
 
 
-# 广度优先， 利用队列
+
+
+# 广度优先， 利用队列, 处理当前节点时, 将子节点入队列
 def bfs(tree: list):
     queue = list()
     root_index = 0
@@ -29,19 +58,12 @@ def bfs(tree: list):
     queue.append(root_node)
     bfs_search(queue, tree)
 
-
 def bfs_search(queue: list, tree: list):
     current_node = queue.pop(0)
     handle(current_node)
     new_queue = extend_children(current_node['index'], queue, tree)
     if len(new_queue):
         bfs_search(new_queue, tree)
-
-
-# 处理
-def handle(node: map):
-    print(node['value'])
-
 
 def extend_children(current_index: int, queue: list, tree: list):
     left_node = get_left(current_index, tree)
@@ -53,24 +75,6 @@ def extend_children(current_index: int, queue: list, tree: list):
     return queue
 
 
-def get_left(current_index: int, tree: list) -> map:
-    try:
-        left_index = 2 * current_index + 1
-        return {'index': left_index, 'value': tree[left_index]}
-    except IndexError:
-        return None
-    except Exception:
-        raise Exception
-
-
-def get_right(index: int, tree: list) -> map:
-    try:
-        right_index = 2 * index + 2
-        return {'index': right_index, 'value': tree[right_index]}
-    except IndexError:
-        return None
-    except Exception:
-        raise Exception
 
 
 if __name__ == '__main__':
